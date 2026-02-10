810f76e8db4890ab927dee13361165fde62e27eb
altyebv <altyeb.404@gmail.com>
2026-02-11 01:21:46 +0200

    removed the old methods

--- app/src/main/java/com/zeros/basheer/data/relations/Relations.kt
+++ app/src/main/java/com/zeros/basheer/data/relations/Relations.kt
@@ -5,10 +5,14 @@ import androidx.room.Junction
 import androidx.room.Relation
 import com.zeros.basheer.data.models.*
 import com.zeros.basheer.feature.concept.data.entity.ConceptEntity
-import com.zeros.basheer.feature.concept.domain.model.Concept
 import com.zeros.basheer.feature.lesson.data.entity.BlockEntity
 import com.zeros.basheer.feature.lesson.data.entity.LessonEntity
 import com.zeros.basheer.feature.lesson.data.entity.SectionEntity
+import com.zeros.basheer.feature.practice.data.entity.SectionConcept
+import com.zeros.basheer.feature.progress.domain.model.UserProgress
+import com.zeros.basheer.feature.quizbank.data.entity.ExamEntity
+import com.zeros.basheer.feature.quizbank.data.entity.QuestionEntity
+import com.zeros.basheer.feature.quizbank.domain.model.Question
 import com.zeros.basheer.feature.subject.data.entity.UnitEntity
 import com.zeros.basheer.feature.subject.domain.model.Subject
 import com.zeros.basheer.feature.subject.domain.model.Units
@@ -106,7 +110,7 @@ data class SubjectWithUnits(
 // ============================================
 
 data class QuestionWithConcepts(
-    @Embedded val question: Question,
+    @Embedded val question: QuestionEntity,
     @Relation(
         parentColumn = "id",
         entityColumn = "id",
@@ -120,7 +124,7 @@ data class QuestionWithConcepts(
 )
 
 data class ExamWithQuestions(
-    @Embedded val exam: Exam,
+    @Embedded val exam: ExamEntity,
     @Relation(
         entity = Question::class,
         parentColumn = "id",
@@ -147,13 +151,6 @@ data class QuizAttemptWithResponses(
 // PROGRESS RELATIONS
 // ============================================
 
-data class LessonWithProgress(
-    @Embedded val lessonEntity: LessonEntity,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "lessonId"
-    )
-    val progress: UserProgress?
-)
+
 
 
