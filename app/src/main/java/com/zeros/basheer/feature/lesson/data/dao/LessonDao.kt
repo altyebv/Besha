f29f5e9c09841967d4d04ef13fbcca1629b1bcdb
altyebv <altyeb.404@gmail.com>
2026-02-11 01:26:36 +0200

    include new methods

--- app/src/main/java/com/zeros/basheer/feature/lesson/data/dao/LessonDao.kt
+++ app/src/main/java/com/zeros/basheer/feature/lesson/data/dao/LessonDao.kt
@@ -7,9 +7,9 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import com.zeros.basheer.data.relations.LessonFull
-import com.zeros.basheer.data.relations.LessonWithProgress
 import com.zeros.basheer.data.relations.LessonWithSections
 import com.zeros.basheer.feature.lesson.data.entity.LessonEntity
+import com.zeros.basheer.feature.lesson.data.relations.LessonWithProgress
 import kotlinx.coroutines.flow.Flow
 
 @Dao
