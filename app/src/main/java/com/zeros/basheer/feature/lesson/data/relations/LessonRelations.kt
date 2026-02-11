fb2d710e5c735e25db2f6e328676cff97537e996
altyebv <altyeb.404@gmail.com>
2026-02-11 15:18:48 +0200

    migrated from the general realation module

--- app/src/main/java/com/zeros/basheer/feature/lesson/data/relations/LessonRelations.kt
+++ app/src/main/java/com/zeros/basheer/feature/lesson/data/relations/LessonRelations.kt
@@ -2,10 +2,14 @@ package com.zeros.basheer.feature.lesson.data.relations
 
 
 import androidx.room.Embedded
+import androidx.room.Junction
 import androidx.room.Relation
+import com.zeros.basheer.feature.concept.data.entity.ConceptEntity
 import com.zeros.basheer.feature.lesson.data.entity.*
+import com.zeros.basheer.feature.practice.data.entity.SectionConcept
 import com.zeros.basheer.feature.progress.data.entity.UserProgressEntity
-import com.zeros.basheer.feature.progress.domain.model.UserProgress
+import com.zeros.basheer.feature.quizbank.data.entity.QuestionConceptEntity
+import com.zeros.basheer.feature.quizbank.data.entity.QuestionEntity
 
 data class LessonWithSections(
     @Embedded val lesson: LessonEntity,
@@ -14,6 +18,21 @@ data class LessonWithSections(
         entityColumn = "lessonId"
     )
     val sections: List<SectionEntity>
+)
+
+
+data class SectionWithConcepts(
+   @Embedded val sectionEntity: SectionEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            value = SectionConcept::class,
+            parentColumn = "sectionId",
+            entityColumn = "conceptId"
+        )
+    )
+    val concepts: List<ConceptEntity>
 )
 
 data class SectionWithBlocks(
@@ -33,4 +52,49 @@ data class LessonWithProgress(
     )
     val progress: UserProgressEntity?
 )
+
+data class LessonFull(
+    @Embedded val lessonEntity: LessonEntity,
+    @Relation(
+        entity = SectionEntity::class,
+        parentColumn = "id",
+        entityColumn = "lessonId"
+    )
+    val sections: List<SectionWithBlocksAndConcepts>
+)
+
+data class QuestionWithConcepts(
+    @Embedded val question: QuestionEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            value = QuestionConceptEntity::class,
+            parentColumn = "questionId",
+            entityColumn = "conceptId"
+        )
+    )
+    val concepts: List<ConceptEntity>
+)
+
+data class SectionWithBlocksAndConcepts(
+    @Embedded val sectionEntity: SectionEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "sectionId"
+    )
+    val blocks: List<BlockEntity>,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id",
+        associateBy = Junction(
+            value = SectionConcept::class,
+            parentColumn = "sectionId",
+            entityColumn = "conceptId"
+        )
+    )
+    val concepts: List<ConceptEntity>
+)
+
+
 
