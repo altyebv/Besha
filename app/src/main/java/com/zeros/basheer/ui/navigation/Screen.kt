0a4a7402920f682ff72a7e41f3a27a9165761501
altyebv <altyeb.404@gmail.com>
2026-02-14 16:24:23 +0200

    fixed navigation , pass subject from main screen

--- app/src/main/java/com/zeros/basheer/ui/navigation/Screen.kt
+++ app/src/main/java/com/zeros/basheer/ui/navigation/Screen.kt
@@ -2,7 +2,9 @@ package com.zeros.basheer.ui.navigation
 
 sealed class Screen(val route: String) {
     object Main : Screen("main")
-    object Lessons : Screen("lessons")
+    object Lessons : Screen("lessons/{subjectId}") {
+        fun createRoute(subjectId: String) = "lessons/$subjectId"
+    }
     object Feeds : Screen("feeds")
     object QuizBank : Screen("quizbank")
     object Profile : Screen("profile")
