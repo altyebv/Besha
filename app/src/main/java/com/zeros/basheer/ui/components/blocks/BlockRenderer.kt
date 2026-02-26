70e842b96c2133b1e3615440579965e4accdc0ad
altyebv <altyeb.404@gmail.com>
2026-02-26 23:41:27 +0200

    Updated with Katex support

--- app/src/main/java/com/zeros/basheer/ui/components/blocks/BlockRenderer.kt
+++ app/src/main/java/com/zeros/basheer/ui/components/blocks/BlockRenderer.kt
@@ -2,81 +2,88 @@ package com.zeros.basheer.ui.components.blocks
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.zeros.basheer.feature.lesson.data.entity.BlockType
+import com.zeros.basheer.core.math.KatexRenderer
 import com.zeros.basheer.domain.model.BlockUiModel
+import com.zeros.basheer.feature.lesson.data.entity.BlockType
 
 /**
  * Central dispatcher for rendering blocks based on their type.
- * 
+ *
  * This is the single entry point for all block rendering.
  * Add new block types here as they're implemented.
+ *
+ * @param katexRenderer The singleton [KatexRenderer], injected into the
+ *                      calling ViewModel and passed down so [FormulaBlock]
+ *                      stays DI-free and testable.
  */
 @Composable
 fun BlockRenderer(
     block: BlockUiModel,
     onConceptClick: (conceptId: String) -> Unit,
+    katexRenderer: KatexRenderer,
     modifier: Modifier = Modifier
 ) {
     when (block.type) {
         BlockType.TEXT -> TextBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.HEADING -> HeadingBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.IMAGE -> ImageBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.GIF -> ImageBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.HIGHLIGHT_BOX -> HighlightBox(
-            block = block,
+            block          = block,
             onConceptClick = onConceptClick,
-            modifier = modifier
+            modifier       = modifier
         )
-        
+
         BlockType.LIST -> ListBlock(
-            block = block,
+            block          = block,
             onConceptClick = onConceptClick,
-            modifier = modifier
+            modifier       = modifier
         )
-        
+
         BlockType.TIP -> TipBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.EXAMPLE -> ExampleBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.FORMULA -> FormulaBlock(
-            block = block,
-            modifier = modifier
+            block     = block,
+            renderer  = katexRenderer,
+            modifier  = modifier
         )
-        
+
         BlockType.TABLE -> TableBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.QUOTE -> QuoteBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
-        
+
         BlockType.DIVIDER -> DividerBlock(
-            block = block,
+            block    = block,
             modifier = modifier
         )
     }
